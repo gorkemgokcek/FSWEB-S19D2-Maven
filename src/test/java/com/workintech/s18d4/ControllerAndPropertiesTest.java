9c163206cec3bb9db90133f75f213e6887e49ad8
Gorkem Gokcek <gorkemgokcekk@gmail.com>
2024-05-13 20:49:03 +0300

    Project completed.

--- src/test/java/com/workintech/s18d4/ControllerAndPropertiesTest.java
+++ src/test/java/com/workintech/s18d4/ControllerAndPropertiesTest.java
@@ -1,13 +1,10 @@
 package com.workintech.s18d4;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.workintech.s18d4.controller.AccountController;
-import com.workintech.s18d4.controller.CustomerController;
 import com.workintech.s18d4.dto.CustomerResponse;
 import com.workintech.s18d4.entity.Account;
 import com.workintech.s18d4.entity.Customer;
 import com.workintech.s18d4.service.AccountService;
-import com.workintech.s18d4.service.CustomerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
